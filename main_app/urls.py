c7eb08bffc298addbc9b73a4ec8552043f9d1592
generalGreg <thompson.gregory.mark@gmail.com>
2025-10-19 04:53:14 -0700

    Added the details page.

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('armies/', views.army_index, name='army-index'),
+    path('armies/<int:army_id>/', views.army_detail, name='army-detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -16,3 +16,7 @@ def army_index(request):
     armies = Army.objects.all()
     # Render the armies/index.html template with the armies data
     return render(request, 'armies/index.html', {'armies': armies})
+
+def army_detail(request, army_id):
+    army = Army.objects.get(id=army_id)
+    return render(request, 'armies/detail.html', {'army': army})
